76f13df261803de33a610719d997351b79d30a47
hyoushke <hyoushke@gmail.com>
2019-07-15 04:49:05 +0800

    Created and setup basic structures of the Todo component

--- src/app/components/todo/todo-item/todo-item.component.ts
+++ src/app/components/todo/todo-item/todo-item.component.ts
@@ -2,8 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
-  templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.sass']
+  //templateUrl: './todo-item.component.html',
+  //styleUrls: ['./todo-item.component.sass']
+
+  template: 
+  `
+    <div>
+      <span>Hello World</span>
+      <img src="" style="width: 200px;" />
+      <div><button>Edit</button></div>
+      <div><button>Remove</button></div>
+      <div><button>Set Enable</button></div>
+      <div><button>Set Color</button></div>
+    </div>
+  `,
+  styles: [
+            ``,
+            ``,
+            ``,                        
+          ]
 })
 export class TodoItemComponent implements OnInit {
 
