c8e0d28703edc379739bfbac17533879ff0a095c
hyoushke <hyoushke@gmail.com>
2019-07-24 11:48:20 +0800

    Enhance the gui of TodoApplication components

--- src/app/components/todo/todo-navigation/todo-navigation.component.ts
+++ src/app/components/todo/todo-navigation/todo-navigation.component.ts
@@ -2,17 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-navigation',
-  template: `
-  <p>
-      <button>Delete</button>
-      <button (click)="onClickCreate()">Create</button>
-      <button>Search</button>
-      <button>Download</button>
-      <button>Export</button>
-  </p>
-  
-  `,
-   //./todo-navigation.component.html',
+  templateUrl: './todo-navigation.component.html',
   styleUrls: ['./todo-navigation.component.sass']
 })
 export class TodoNavigationComponent implements OnInit {
