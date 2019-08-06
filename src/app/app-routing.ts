5a715993a2f264287455d7e4113762bf2ecb2431
hyoushke <hyoushke@gmail.com>
2019-08-06 15:28:05 +0800

    Configured routes for login and register

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -3,11 +3,15 @@ import {Routes, RouterModule} from '@angular/router';
 
 import {TodoContainerComponent} from './components/todo/todo-container/todo-container.component';
 import {PostComponent} from './components/post/post/post.component';
+import {LoginComponent} from './components/login/login/login.component';
+import {RegisterComponent} from './components/register/register/register.component';
 
 const appRoutes: Routes = [
 
         {path: 'todo', component: TodoContainerComponent  },
         {path: 'post', component: PostComponent  },
+        {path: 'login', component: LoginComponent  },
+        {path: 'register', component: RegisterComponent  },
 
 ]
 
