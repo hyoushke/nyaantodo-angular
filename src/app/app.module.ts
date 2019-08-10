1ba8bbf8c9efa3fcea6b8d5cd787e2080edf73e1
hyoushke <hyoushke@gmail.com>
2019-08-09 23:13:31 -0700

    Imported the ReactiveFormsModule in src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -68,6 +68,7 @@ import { TodoReactiveformComponent } from './components/todo/todo-reactiveform/t
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     routing
