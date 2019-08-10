a75f28b7124a60302734d289dca30870522a2b1b
hyoushke <hyoushke@gmail.com>
2019-08-10 00:33:13 -0700

    Setup Validation getters for Reactive Form component

--- src/app/components/todo/todo-reactiveform/todo-reactiveform.component.ts
+++ src/app/components/todo/todo-reactiveform/todo-reactiveform.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TodosService } from 'src/app/services/todos.service';
 import { IPost } from 'src/app/interface/IPost';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 
 
@@ -23,18 +23,18 @@ export class TodoReactiveformComponent implements OnInit {
 
   ngOnInit() {
     this.formPosts = this.formBuilder.group({
-      author: null,
-      title: null,
-      content: null,
-      categories: null,
-      tags: null,
-      likes: null,
-      subscribers: null,
-      shares: null,
-      views: null,
-      imageurl: null,
-      datecreated: null,
-      datemodified: null
+      author: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      content: [null, [Validators.required]],
+      categories: [null, [Validators.required]],
+      tags: [null, [Validators.required]],
+      likes: [null, [Validators.required]],
+      subscribers: [null, [Validators.required]],
+      shares: [null, [Validators.required]],
+      views: [null, [Validators.required]],
+      imageurl: [null, [Validators.required]],
+      datecreated: [null, [Validators.required]],
+      datemodified: [null, [Validators.required]]
     });
 
     this.logFormPostValue();
@@ -86,18 +86,56 @@ export class TodoReactiveformComponent implements OnInit {
 
 
 onClickCreatePost(){
+    let data = {
+                  message: "broadcast from todo form child [successfully created new todo]",
+                  counter: 44,
+                  navigation: 'TDO_REACTIVEFORM'
+               };
+
+              //this.todoReactiveFormEvent.emit(data);
+              //this.addPost();
+  }
 
 
-let data = {
-message: "broadcast from todo form child [successfully created new todo]",
-counter: 44,
-navigation: 'TDO_REACTIVEFORM'
-};
 
-this.todoReactiveFormEvent.emit(data);
-this.addPost();
+  //validation getters in UI
+  get author(){
+    return this.formPosts.get('author');
+  }
+  get title(){
+    return this.formPosts.get('title');
+  }
+  get content(){
+    return this.formPosts.get('content');
+  }
+  get categories(){
+    return this.formPosts.get('categories');
+  }
+  get tags(){
+    return this.formPosts.get('tags');
+  }
+  get likes(){
+    return this.formPosts.get('likes');
+  }
+  get subscribers(){
+    return this.formPosts.get('subscribers');
+  }
+  get shares(){
+    return this.formPosts.get('shares');
+  }
+  get views(){
+    return this.formPosts.get('views');
+  }
+  get imageurl(){
+    return this.formPosts.get('imageurl');
+  }
+  get datecreated(){
+    return this.formPosts.get('datecreated');
+  }
+  get datemodified(){
+    return this.formPosts.get('datemodified');
+  }
 
-}
 
 
 }
